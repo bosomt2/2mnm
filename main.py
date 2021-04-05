bf3f300d84b00a2f3603804094505ed5d42d7300
testing <testing@testings-MacBook-Air.local>
2021-04-05 14:33:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+mm_count = 5  # random.randint(1,100)
+
+attempt_limit = 5
+attempts = 0
+
+print("davaj")
+
+while attempts < attempt_limit:
+    guess_text = input("kolko?")
+    guess = int(guess_text)
+    if mm_count == guess:
+        print(f"ano! bolo to {guess}")
+    elif guess < mm_count:
+        print("malo")
+    else:
+        print("privela")
+    attempts += 1
+
+print("bye")
+print(mm_count)
